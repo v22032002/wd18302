c985dfc02d209db0a9da3928502da31464a3e92a
trungnt173 <trungnt173@fpt.edu.vn>
2024-07-20 18:05:20 +0700

    Update code 20/7

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,43 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/posts', function () {
+    // truy vấn
+    // Lấy tất cả
+//    $data = Post::all();
+    // c2
+    $data = Post::query()->get();
+    // where
+    $data = Post::query()
+        ->where('id', '>=', 1)
+        ->get();
+    // Them
+    // C1
+//        $post = new Post();
+//        $post->title = "BV 2";
+//        $post->content = "ND BV so 2";
+//        $post->save();
+//        C2
+//    $post = Post::query()->create([
+//        'title' => "BV so 3",
+//        'content' => "ND BV so 3",
+//        'name' => "Nguyen Van A"
+//    ]);
+    // Sua
+    // C1
+//    $post = Post::query()->find(1);
+//    $post->title = "BV 2";
+//    $post->save();
+//    C2
+    $post = Post::query()->find(1)
+        ->update([
+            'title' => "BV so 3",
+            'content' => "ND BV so 3"
+        ]);
+    // Xoa
+//  Cung
+    $post = Post::query()->find(1)->delete();
+    dd($data);
+//    return view('welcome');
 });
